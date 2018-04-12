1ae0b23fb1112234a5955bd6893dbda113e8b898
Nicholas Jacquet <nicholasjacquet2@gmail.com>
2018-04-12 12:48:10 -0500

    Update FullScreenFrame.java

--- Projects/FullScreenFrame.java
+++ Projects/FullScreenFrame.java
@@ -3,28 +3,18 @@ import java.awt.event.*;
 import javax.swing.JFrame;
 
 public class FullScreenFrame extends JFrame implements KeyListener {
-
     public FullScreenFrame () {
         addKeyListener(this);
-        //setUndecorated(true);
         GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
-
         if (gd.isFullScreenSupported()) {
-            //try {
 		setUndecorated(true);
                 gd.setFullScreenWindow(this);
-            //}
-            //finally {
-            //    gd.setFullScreenWindow(null);
-            //}
         }
         else {
             System.err.println("Full screen not supported");
         }
-
         setVisible(true);
     }
-
     public void keyTyped(KeyEvent e) {}
     public void keyPressed(KeyEvent e) {}
     public void keyReleased(KeyEvent e) {
